7fbd4e668c4fb327f2431f4d364b7993c3b36137
Julia Zdzilowska <135221630+jzdzilowska@users.noreply.github.com>
2025-01-14 02:19:52 +0100

    feat: 3d model working; everything updated

--- src/main.js
+++ src/main.js
@@ -1,22 +1,68 @@
-import * as THREE from 'three';
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader"
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new THREE.Scene()
+const light = new THREE.SpotLight()
+light.position.set(20, 20, 20)  
+scene.add(light)
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+const camera = new THREE.PerspectiveCamera(
+  75,  
+  window.innerWidth / window.innerHeight,  
+  0.1,  
+  1000  
+)
+camera.position.z = 40  
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true  // Enable smooth controls
 
-camera.position.z = 5;
+const material = new THREE.MeshBasicMaterial({
+  color: 0xffffff, 
+  wireframe: true,  
+  opacity: 0.5,  
+})
+
+// PLY model
+const loader = new PLYLoader()
+loader.load(
+  "/earth21.ply",  
+  function(geometry) {
+    geometry.computeVertexNormals()
+    const mesh = new THREE.Mesh(geometry, material)  
+    mesh.rotateX(-Math.PI / 2)  
+    mesh.scale.set(0.00001, 0.00001, 0.00001)  
+    scene.add(mesh)  
+  },
+  xhr => {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded")  // Loading progress
+  },
+  error => {
+    console.log(error)  
+  }
+)
+
+// Handle window resizing
+window.addEventListener("resize", onWindowResize, false)
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight  // Update camera aspect ratio
+  camera.updateProjectionMatrix()  // Recalculate the camera projection matrix
+  renderer.setSize(window.innerWidth, window.innerHeight) 
+  render()  
+}
 
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+  requestAnimationFrame(animate)  // Request the next frame
+  controls.update()  
+  render()  
 }
-renderer.setAnimationLoop( animate );
+
+function render() {
+  renderer.render(scene, camera)
+}
+
+animate() 
